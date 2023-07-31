d61e60ef7013a4790ae67054c4d0f7cb5936c0bf
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 10:14:16 +0900

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -1,3 +1,7 @@
 export default eventHandler(() => {
-  return { nitro: 'Is Awesome!' }
+  return new Response(`<!doctype HTML><html><head><meta charset="utf-8"></head><body><h1>sw-proxy</h1></body></html>`, {
+    headers: {
+      "content-type": "text/html",
+    }
+  })
 })
