d3c9c542ddaa819a361f1aa0db6acbf3a3595037
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 09:22:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-alert('OK')
-try {
-  const registration = await navigator.serviceWorker.register('/sw-proxy/sw.js', {
-    scope: '/sw-proxy',
-  })
-} catch(e) {
-  alert(e)
-}
+const registration = await navigator.serviceWorker.register('/sw-proxy/sw.js', {
+  scope: '/sw-proxy',
+}).catch(alert)
+
